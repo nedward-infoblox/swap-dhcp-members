0f92a72256b80840defa90a075a3fe70196453c5
nedward-infoblox <nedward@infoblox.com>
2023-01-23 21:33:32 +0530

    Range updater

--- range_update_parent.py
+++ range_update_parent.py
@@ -3,18 +3,16 @@ import requests
 import urllib3
 from sys import argv, exit
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 def prix(str):
-	print(str)
-	exit()
-
+    print(str)
+    exit()
 def outex(st):
-	with open('output.csv', 'w', newline='') as csvfile:
-		spamwriter = csv.writer(csvfile)
-		spamwriter.writerows(st)
-	csvfile.close()
-
-
+    with open('output.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile)
+        spamwriter.writerows(st)
+    csvfile.close()
+    exit()
+output = []
 with open('ranges.csv', 'r') as f:
     reader = csv.reader(f)
     ranges = list(reader)
@@ -23,13 +21,15 @@ server_address = '172.20.24.61'
 gm_user = 'M554644'
 gm_pwd = 'Welcome@123'
 
-
 s = ranges[0].index("start_address*")
 p = ranges[0].index("EA-parent_network")
 
+output.extend([ranges[0]])
 for r in ranges[2:]:
-    url1='https://{}/wapi/v2.9/range?start_address={}'.format(server_address,r[s])
+    url1='https://{}/wapi/v2.9/range?start_addr={}'.format(server_address,r[s])
     response1 = requests.get(url1, verify=False, auth=(gm_user, gm_pwd)).json()
     if len(response1) != 0:
-        r[p] = response1[0]['start_addr']
-
+        r[p] = response1[0]['network'].split('/')[0]
+        output.append(r)
+    print(r[s]+'    '+r[p]+'        ', end='\r')
+outex(output)
