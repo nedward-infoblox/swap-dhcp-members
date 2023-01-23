4b47a5e4f0696b2ffc5d300f01c4f541fc617760
nedward-infoblox <nedward@infoblox.com>
2023-01-23 21:18:16 +0530

    Adding new files

--- csv_modder.py
+++ csv_modder.py
@@ -1,11 +1,11 @@
 import csv
-import requests
-import urllib3
+# import requests
+# import urllib3
 from sys import argv, exit
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def outex(st):
-	with open('output.csv', 'w', newline='') as csvfile:
+	with open(argv[3], 'w', newline='') as csvfile:
 		spamwriter = csv.writer(csvfile)
 		spamwriter.writerows(st)
 	csvfile.close()
@@ -55,40 +55,32 @@ def modify_csv(networks_csv, ranges_csv, foa_csv, arg1, arg2):
 		reader = csv.reader(f)
 		lines = list(reader)
 
-	server_address = '172.20.24.61'
-	gm_user = 'M554644'
-	gm_pwd = 'Welcome@123'
-
-	startIP = []
-	for n in networks:
-		url1='https://{}/wapi/v2.9/range?network={}'.format(server_address,n)
-		response1 = requests.get(url1, verify=False, auth=(gm_user, gm_pwd)).json()
-		if len(response1) != 0:
-			startIP.append(response1[0]['start_addr'])
-
 	# Find indices
 	j = lines[0].index("failover_association")
-	s = lines[0].index("start_address*")
+	s = lines[0].index("EA-parent_network")
 	m = lines[0].index("member")
+	sa = lines[0].index("server_association_type")
 
+	lines[0][s] = ''
 	output.extend([lines[0]])
 	for r in lines[1:]:
 		try:
 			# Check if start_address is present in list of start IPs, if yes replace foa with new foa
-			if r[s] in startIP:
-				newr = list(map(lambda x: x.replace(r[j], failoverAssociation), r))
-				newr = list(map(lambda x: x.replace(newr[m], arg1), newr))
-				if newr[0] == "dhcprange":
-					output.append(newr)
+			if r[0] == 'dhcprange' and r[s] in networks:
+				r[j] = failoverAssociation
+				r[sa] = 'FAILOVER'
+				r[s] = ''
+				output.append(r)
 		except Exception as e:
 			print("Error: ", e)
-
+	# print(output)
 	return output
 
 # Calling function
-result1 = modify_csv('networks.csv', 'ranges.csv', 'foa.csv', argv[1], argv[2])
-result2 = modify_csv('networks.csv', 'ranges.csv', 'foa.csv', argv[1], argv[2])
+result1 = modify_csv('networks.csv', 'ranges_updated.csv', 'foa.csv', argv[1], argv[2])
+result2 = modify_csv('networks.csv', 'ranges_updated.csv', 'foa.csv', argv[1], argv[2])
 
+# prix(result1)
 for entry in result1[1:]:
 	if entry[0] == "network":
 		entry[i] = entry[i].replace(argv[1], '').replace(',,', ',').strip(',')
---
+++ range_update_parent.py
@@ -0,0 +1,35 @@
+import csv
+import requests
+import urllib3
+from sys import argv, exit
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def prix(str):
+	print(str)
+	exit()
+
+def outex(st):
+	with open('output.csv', 'w', newline='') as csvfile:
+		spamwriter = csv.writer(csvfile)
+		spamwriter.writerows(st)
+	csvfile.close()
+
+
+with open('ranges.csv', 'r') as f:
+    reader = csv.reader(f)
+    ranges = list(reader)
+
+server_address = '172.20.24.61'
+gm_user = 'M554644'
+gm_pwd = 'Welcome@123'
+
+
+s = ranges[0].index("start_address*")
+p = ranges[0].index("EA-parent_network")
+
+for r in ranges[2:]:
+    url1='https://{}/wapi/v2.9/range?start_address={}'.format(server_address,r[s])
+    response1 = requests.get(url1, verify=False, auth=(gm_user, gm_pwd)).json()
+    if len(response1) != 0:
+        r[p] = response1[0]['start_addr']
+
